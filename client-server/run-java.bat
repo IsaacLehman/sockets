45a291817ea43957b47a119e597e7e70ceb5d3e3
IsaacLehman <iikkeelloo@gmail.com>
2021-04-09 14:25:11 -0400

    More comment changes

--- client-server/run-java.bat
+++ client-server/run-java.bat
@@ -7,11 +7,14 @@ echo Available .java Files:
 for %%f in ("*.java") do @echo - %%~nf
 echo.
 echo ----------------------------------------------------------------
+
 :: Get the java file name <file_name>.java
 set /p file_name=Please enter the file-name (file-name.java):
 echo.
+
 :: create the full file name with ".java" extension
 SET full_file_name=%file_name%.java
+
 :: compile and run
 javac %full_file_name% && (
   echo %full_file_name% was compiled succesfully.
@@ -21,5 +24,6 @@ javac %full_file_name% && (
   echo.
   echo ERROR: Could not compile %full_file_name%.
 )
+
 :: don't close terminal until the user presses a key
 pause
